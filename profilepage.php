3f49d3e746ae53c459db413cb391c361502ea68b
Samreen Hunagund <133121963+1Samreen@users.noreply.github.com>
2023-09-22 18:01:04 +0530

    profilepage.php

--- profilepage.php
+++ profilepage.php
@@ -8,7 +8,7 @@ session_start();
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<title>Musical Connection</title>
+		<title>Ideal Bits</title>
  		<link rel="stylesheet" href="css/style.css" type="text/css" />
  		<script type="text/javascript" src="Script/jquery.js"></script>
 		<link rel="stylesheet" href="css/jquery-ui.css">
@@ -254,16 +254,16 @@ session_start();
 			</div>
 			<div class="right-content">
 				<h1 style="margin-top:32px;text-decoration:underline">Latest Upcoming Events </h1>
-					<h2>New York City Music festival</h2>
-					<h2>November 30 ,2014 </h2>
-					<h2>Venue : Madison Square Garden</h2>
+					<h2>Bangalore City Music festival</h2>
+					<h2>September 22 ,2023 </h2>
+					<h2>Venue : HKBK College </h2>
 			</div>
 
 		</div>
 		<div class="footer">
 			<div class="left-content wifooterdthalign">
 				<h1>Subscribe to Us</h1>
-				<a href="mailto:amitk26467@gmail.com">musicalConnections@gmail.com</a>
+				<a href="mailto:amitk26467@gmail.com">idealbits@gmail.com</a>
 			</div>
 			<div class="left-content wifooterdthalign" style="margin-left:0px">
 				<h1>Contact Us</h1>
@@ -271,13 +271,13 @@ session_start();
 			</div>
 			<div class="left-content social-network" style="margin-left:0px">
 				<h1>Follow Us</h1>
-				<a href="https://www.facebook.com/Connectionsoriginalmusicall" class="socialnet" style="clear:left"></a>
+				<a href="https://www.facebook.com/idealbitsmusical" class="socialnet" style="clear:left"></a>
 				<a href="https://twitter.com/music" class="socialnet twitter"></a>
 				<a href="http://www.youtube.com/user/FlyingBalalaikaBros" class="socialnet youtube"></a>
 			</div>
 			<div class="left-content wifooterdthalign" style="margin-left:0px">
-				<h2 style="margin-top:0px;font-size:14px">Copyrights reserved 2014.</h2>
-				<h2 style="margin-top:5px;font-size:10px">Developed and managed by Amit and Chaithra</h2>
+				<h2 style="margin-top:0px;font-size:14px">Copyrights reserved 2023.</h2>
+				<h2 style="margin-top:5px;font-size:10px">Developed and managed by Samreen and Sahana and Swati</h2>
 
 
 			</div>
